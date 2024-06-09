4ff775418b5c1c47699dbaff54f9d640de727f1c
Harry <harry.warner@iventis.co.uk>
2024-06-09 16:41:36 +0100

    move key

--- src/components/key.tsx
+++ src/components/key.tsx
@@ -12,11 +12,11 @@ export const Key = ({ genreColours }: KeyProps) => {
 
     return (
         <>
-            <div className="fixed bottom-10 right-10 bg-white p-1 rounded-md shadow-lg w-12 h-11 flex items-center justify-center">
+            <div className="fixed bottom-5 right-5 bg-white p-1 rounded-md shadow-lg w-12 h-11 flex items-center justify-center">
                 <button onClick={() => setIsOpen(!isOpen)}>Key</button>
             </div>
             {isOpen && (
-                <div className="absolute right-10 bottom-24 bg-white rounded-md p-4 shadow-lg flex flex-col gap-1">
+                <div className="absolute right-5 bottom-16 my-1 bg-white rounded-md p-4 shadow-lg flex flex-col gap-1">
                     {Object.entries(genreColours).map(([genre, colour]) => (
                         <div key={genre} className="flex items-center gap-2">
                             <div className="w-3 h-3" style={{ backgroundColor: colour }}/>
