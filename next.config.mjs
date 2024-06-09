612c446e8acd7cddcd4d328de8db72ebe3124ff5
Harry <harry.warner@iventis.co.uk>
2024-06-09 12:38:39 +0100

    Add csv loader

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config) => {
+        return {
+            ...config,
+            module: {
+                ...config.module,
+                rules: [
+                    ...config.module.rules,
+                  {
+                    test: /\.csv$/,
+                    loader: 'csv-loader',
+                    options: {
+                      dynamicTyping: true,
+                      header: true,
+                      skipEmptyLines: true
+                    }
+                  }
+                ]
+              }
+        }
+    }
+};
 
 export default nextConfig;
