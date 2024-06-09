0f1490bd241b2c052180d61b45cc2620a03fc9e6
Harry <harry.warner@iventis.co.uk>
2024-06-09 16:10:02 +0100

    Fix styling

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -14,10 +14,10 @@ export const NavBar = () => {
     <>
         <div className="grid grid-flow-col auto-cols-fr w-screen fixed bg-sky-500">
             {days.map((day) => (
-                <Link key={day} className={`${pathname === getPathNameForDay(day.toLowerCase()) ? "bg-sky-200" : "text-white"} h-8 flex items-center justify-center w-100`} href={getPathNameForDay(day)}>{day}</Link>
+                <Link key={day} className={`${pathname === getPathNameForDay(day.toLowerCase()) ? "bg-sky-200" : "text-white"} h-10 flex items-center justify-center w-100`} href={getPathNameForDay(day)}>{day}</Link>
             ))}
         </div>
-        <div className="min-h-8 w-screen"/>
+        <div className="min-h-10 w-screen"/>
     </>
   )
 }
